11cff0c82cf0621fdb8b2b2463b24606826dd926
Glint_19373019 <2533475336@qq.com>
2023-01-31 10:12:30 +0800

    通讯配置

---
+++ assets/js/apikey.js
@@ -0,0 +1,4 @@
+export default {
+  USER_ID :'rnVbmcYqjSuK_oPfH',
+  TEMPLATE_ID:'template_ddf2xzx'
+}
--- assets/js/sendMessage.js
+++ assets/js/sendMessage.js
@@ -1,21 +1,26 @@
 import 'email.min.js'
+import emailjs from 'https://cdn.jsdelivr.net/npm/emailjs-com@2/dist/email.min.js'
+import apiKeys from 'apikeys.js'
 
 const sendButton = document.getElementById('send-button')
 const sendMessage = document.getElementById('message').value
 const sendName = document.getElementById('name').value
 const sendEmail = document.getElementById('email').value
-sendButton.addEventListener('click', function() {
-  if(sendName && sendEmail && sendMessage){
-    emailjs.init('rnVbmcYqjSuK_oPfH')
-    var message = {
-      name: sendName,
-      email: sendEmail,
-      message: sendMessage,
-    }
-    emailjs.send('service_1','rnVbmcYqjSuK_oPfH', message).then(function(response) {
-      console.log('SUCCESS!', response.status, response.text);
-    }, function(err) {
-      console.log('FAILED...', err);
-    })
-  }
-})
+
+const sendMove = e => {
+  e.preventDefault()
+
+  emailjs
+    .sendForm('gmail', apiKeys.TEMPLATE_ID, e.target, apiKeys.USER_ID)
+    .then(
+      result => {
+        console.log(result.text)
+        alert('发送成功')
+      },
+      error => {
+        console.log(error.text)
+        alert('发送失败')
+      }
+    )
+}
+sendButton.addEventListener('submit', sendMove)
