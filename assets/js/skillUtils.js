3b7f86fa8d6e7adb601b88fffe0c0fed0b8d0094
GlintFreedom <glint.freedom@gmail.com>
2024-08-31 14:25:31 +0800

    Fixed the width of the first column "Effects" in the technical page

--- assets/js/skillUtils.js
+++ assets/js/skillUtils.js
@@ -2,7 +2,7 @@ const filePath = 'assets/demo/';
 const filePairs = {
   'button1': [filePath+'button1.js', filePath+'button1.css'],
   // 'animation1': [filePath+'animation1.js', filePath+'animation1.css'],
-  // 'switch1': [filePath+'switch1.js', filePath+'switch1.css'],
+  'switch1': [filePath+'switch1.js', filePath+'switch1.css'],
 };
 
 // 函数：加载文件内容并拼接
